70ff648a6c11dfb45a554fe9548aaa6bc8c776e7
Marcio Okidoi <okidoi@gmail.com>
2019-08-08 22:54:35 -0300

    TPL Admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php"); //composer
 
 use \Slim\Slim;    //namespaces dentro do vendor
 use \Hcode\Page;  //namespaces dentro do vendor
+use \Hcode\PageAdmin;  
 
 $app = new Slim();   // Slim Framework
 
@@ -12,6 +13,14 @@ $app->config('debug', true);
 $app->get('/', function() {   //Rota
     
 	$page = new Page();
+	
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {   //Rota
+    
+	$page = new PageAdmin();
 	
 	$page->setTpl("index");
 
