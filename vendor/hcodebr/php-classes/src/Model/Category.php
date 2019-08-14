cbffdc236a596476777cc494046fa94417ddb01c
Marcio Okidoi <okidoi@gmail.com>
2019-08-13 22:22:56 -0300

    Site - Categorias

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -28,6 +28,8 @@ class Category extends Model{
 				":descategory"=>$this->getdescategory()
 		));
 		$this->setData($results[0]);
+
+		Category::updateFile(); //Depois de alterar as categorias já as atualizo no site
 	}
 
 	public function get($idcategory)
@@ -50,6 +52,20 @@ class Category extends Model{
 			':idcategory'=>$this->getidcategory()
 		]);
 		
+		Category::updateFile();
+	}
+
+	public static function updateFile(){
+
+		$categories = Category::listAll();
+
+		$html = [];
+
+		foreach($categories as $row){
+			array_push($html, '<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory']. ' </a></li>');
+		}
+
+		file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views". DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html));
 	}
 	
 }
